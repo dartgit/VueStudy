82aefc45d9f336183ca636b76def63228316e99d
dartgit <fvzanqueta@hotmail.com>
2022-12-10 18:38:19 -0300

    Ajuste Layout

--- vueproject/src/ExpQuasar.vue
+++ vueproject/src/ExpQuasar.vue
@@ -1,26 +1,30 @@
 <template>
-  <div class="buttons-column">
-    <div class="column-header">
-      QUASAR
+  <div>
+    <div class="buttons-columnq">
+      <div class="column-headerq">
+        QUASAR
+      </div>
     </div>
-    <div>
-      <q-btn round color="white"  text-color="black"  @click="NotifyClick2">
-        <q-avatar size="42px">
-          <img src="https://scontent.fcgh39-1.fna.fbcdn.net/v/t1.18169-9/11074587_702535033188475_8513073536182128188_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFzbXr_4rEZXMg9M-c92RQLyavowaR7X1LJq-jBpHtfUhC2VSG6MDg-IxYR97dwikgtys0h6K6sUcfvm8VZc0QV&_nc_ohc=kmqzv8OfuBcAX9TCy0y&tn=C-vexXGSJhrw6QlT&_nc_ht=scontent.fcgh39-1.fna&oh=00_AfDkh2YMMGOCf_K5nP2qMUNCS-F2FLhutmHx7oXikQapBw&oe=63AAF677">          
-        </q-avatar>
-      </q-btn>
-    </div>    
-    <div>
-      <q-btn round color="white"  text-color="black"  @click="NotifyClick">
-        <q-avatar size="42px">
-          <img src="https://scontent.fcgh39-1.fna.fbcdn.net/v/t39.30808-6/310478933_2266637613492444_467116057370013632_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFq-iQOCVte4iboL8vRoGrnQs_U3XKm3flCz9Tdcqbd-WhKHmio--NnSiTocvU6mePuDG4KlhzvE5Y9GHP9GNqZ&_nc_ohc=Iay91UDh9rIAX8vfWE4&_nc_ht=scontent.fcgh39-1.fna&oh=00_AfAXIbUeS301w9KWph6dDAQ-4RhpHLzt4MRSJuIfEDUdZg&oe=6387F0A1">          
-        </q-avatar>
-      </q-btn>
+    <div class="buttons-row">
+      <div>
+        <q-btn round color="white"  text-color="black"  @click="NotifyClick2">
+          <q-avatar size="100px">
+            <img src="https://scontent.fcgh39-1.fna.fbcdn.net/v/t1.18169-9/11074587_702535033188475_8513073536182128188_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFzbXr_4rEZXMg9M-c92RQLyavowaR7X1LJq-jBpHtfUhC2VSG6MDg-IxYR97dwikgtys0h6K6sUcfvm8VZc0QV&_nc_ohc=kmqzv8OfuBcAX9TCy0y&tn=C-vexXGSJhrw6QlT&_nc_ht=scontent.fcgh39-1.fna&oh=00_AfDkh2YMMGOCf_K5nP2qMUNCS-F2FLhutmHx7oXikQapBw&oe=63AAF677">          
+          </q-avatar>
+        </q-btn>
+      </div>    
+      <div>
+        <q-btn round color="white" text-color="black"  @click="NotifyClick">
+          <q-avatar size="100px">
+            <img src="https://scontent.fcgh39-1.fna.fbcdn.net/v/t39.30808-6/310478933_2266637613492444_467116057370013632_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFq-iQOCVte4iboL8vRoGrnQs_U3XKm3flCz9Tdcqbd-WhKHmio--NnSiTocvU6mePuDG4KlhzvE5Y9GHP9GNqZ&_nc_ohc=dLB6B_AlaXIAX9LKwPy&_nc_ht=scontent.fcgh39-1.fna&oh=00_AfBVPP4ikUB-PpOycexdxdKL-DdEqK-kaehXDv3T14gicw&oe=6399BCE1">          
+          </q-avatar>
+        </q-btn>
+      </div>      
+      <div>
+        <q-btn color="deep-purple" icon="repartition" glossy label="Voltar" @click="QuasarClick" />
+      </div>             
     </div>      
-    <div>
-      <q-btn color="deep-purple" icon="repartition" glossy label="Voltar" @click="QuasarClick" />
-    </div>             
-  </div>      
+  </div>
 </template>
   
   <script>
@@ -81,24 +85,48 @@
   </script>
   
   <style>
-    .buttons-column > .column-header {
-    flex-grow: 0;
+    .buttons-columnq > .column-headerq {
     width: 150px;
-    height: 35px;
-    font-size: 130%;
-    opacity: 0.6;
-    text-align: left;
+    height: 50px;
+    text-align: center;
     padding-left: 15px;
+    color : rgb(71, 2, 71);
   }
 
-  .buttons-column > div {
+  .buttons-columnq > div {
     width: 150px;
     height: 50px;
     text-align: center;
   }
   
-  .buttons-column {
+  .buttons-columnq {
     width: 150px;
+    height: 50px;
+    font-size: 30px;
+    padding-left: 1px;    
     justify-content: center;
+  }
+  
+  .buttons-row > .row-header {
+    flex-grow: 0;
+    width: 150px;
+    height: 50px;
+    font-size: 130%;
+    opacity: 0.6;
+    text-align: left;
+    padding-left: 15px;
+  }  
+
+  .buttons-row > div {
+    width: 150px;
+    height: 110px;
+    text-align: center;
+  }  
+
+  .buttons-row {
+    width: 150px;
+    height: 300px;
+    padding-left: 20px;        
+    justify-content: center;    
   }
   </style>
