cbed4e6244bd24a6dd04789a0d57ada124746f80
dartgit <fvzanqueta@hotmail.com>
2023-06-15 10:30:54 -0300

    Fotos

--- vueproject/src/ExpQuasar.vue
+++ vueproject/src/ExpQuasar.vue
@@ -9,14 +9,14 @@
       <div>
         <q-btn round color="white"  text-color="black"  @click="NotifyClick2">
           <q-avatar size="100px">
-            <img src="https://scontent.fcgh39-1.fna.fbcdn.net/v/t1.18169-9/11074587_702535033188475_8513073536182128188_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFzbXr_4rEZXMg9M-c92RQLyavowaR7X1LJq-jBpHtfUhC2VSG6MDg-IxYR97dwikgtys0h6K6sUcfvm8VZc0QV&_nc_ohc=kmqzv8OfuBcAX9TCy0y&tn=C-vexXGSJhrw6QlT&_nc_ht=scontent.fcgh39-1.fna&oh=00_AfDkh2YMMGOCf_K5nP2qMUNCS-F2FLhutmHx7oXikQapBw&oe=63AAF677">          
+            <img src="https://scontent.fcgh21-1.fna.fbcdn.net/v/t1.18169-9/11074587_702535033188475_8513073536182128188_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFzbXr_4rEZXMg9M-c92RQLyavowaR7X1LJq-jBpHtfUhC2VSG6MDg-IxYR97dwikgtys0h6K6sUcfvm8VZc0QV&_nc_ohc=jHq484OkZoMAX-VxAuo&_nc_ht=scontent.fcgh21-1.fna&oh=00_AfBqvn21UInHiQbisVRZ7t_7-s_aw3CRGU0coNNvYECBoA&oe=64B26A37">          
           </q-avatar>
         </q-btn>
       </div>    
       <div>
         <q-btn round color="white" text-color="black"  @click="NotifyClick">
           <q-avatar size="100px">
-            <img src="https://scontent.fcgh39-1.fna.fbcdn.net/v/t1.18169-9/12002218_518769281612628_8733454581447385574_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=174925&_nc_eui2=AeFke81C91BHUN1i9Mrv2yphGj6Tqob19nwaPpOqhvX2fBT4i_t2lImfUAoerItYo6BjsAkdcm65iaiqpEmGfm9J&_nc_ohc=57dBJolZsNUAX8Y6A-B&_nc_ht=scontent.fcgh39-1.fna&oh=00_AfBxmL6IxZDpceC_V7feoU5i-WRdyRgpviHrwHdyTbW76A&oe=63C3556E">          
+            <img src="https://scontent.fcgh21-1.fna.fbcdn.net/v/t39.30808-6/310478933_2266637613492444_467116057370013632_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFq-iQOCVte4iboL8vRoGrnQs_U3XKm3flCz9Tdcqbd-WhKHmio--NnSiTocvU6mePuDG4KlhzvE5Y9GHP9GNqZ&_nc_ohc=vWGE8J7E4JgAX-mNPRX&_nc_ht=scontent.fcgh21-1.fna&oh=00_AfBYPMsAzVbQsiXqeqK6nd_DPtM9XyxAv6g5JSF5m_2tLw&oe=6490EE21">          
           </q-avatar>
         </q-btn>
       </div>      
