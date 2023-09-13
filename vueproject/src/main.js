779b5cace0f772285047fe2b6dcfb7f306b8a32e
dartgit <fvzanqueta@hotmail.com>
2023-09-13 19:54:58 -0300

    EstudoPt2

--- vueproject/src/main.js
+++ vueproject/src/main.js
@@ -1,9 +1,10 @@
-import 'devextreme/dist/css/dx.common.css';
-import 'devextreme/dist/css/dx.light.css';
-import { createApp } from 'vue'
-import App from './LoginForm.vue'
-import { Quasar } from 'quasar'
-import quasarUserOptions from './quasar-user-options'
-import createStore from './store'
+import "devextreme/dist/css/dx.common.css";
+import "devextreme/dist/css/dx.light.css";
+import { createApp } from "vue";
+//import App from "./LoginForm.vue";
+import App from "./PageEstudo.vue";
+import { Quasar } from "quasar";
+import quasarUserOptions from "./quasar-user-options";
+import createStore from "./store";
 
-createApp(App).use(createStore).use(Quasar, quasarUserOptions).mount('#app')
+createApp(App).use(createStore).use(Quasar, quasarUserOptions).mount("#app");
